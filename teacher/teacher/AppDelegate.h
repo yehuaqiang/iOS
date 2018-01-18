0a119a689a0e5e5d13b0274681f6b8f71a145a29
FDMacMini <FDMacMini@123-mini.local>
2018-01-18 09:19:11 +0800

    第一次提交

---
+++ teacher/teacher/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  teacher
+//
+//  Created by yhq on 17/12/13.
+//  Copyright © 2017年 teacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import Firebase;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,FIRMessagingDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ teacher/teacher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  teacher
+//
+//  Created by yhq on 17/12/13.
+//  Copyright © 2017年 teacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
